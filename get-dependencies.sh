723d5fe0ed43c05b95b6f1eacaa8c94555d12697
Link4Electronics <link4electronics@gmail.com>
2026-03-31 00:56:29 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,11 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm amarok
+pacman -Syu --noconfirm \
+    amarok        \
+    lxqt-qtplugin \
+    kvantum       \
+    qt6ct
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
